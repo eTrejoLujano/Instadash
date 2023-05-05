5aad07b8620057378cca866bf30bbc4285419fa3
Erik Trejo Lujano <trejolujano.erik@gmail.com>
2023-05-04 22:08:22 -0700

    category and food type bars match small screen

--- src/components/CategoryBar.jsx
+++ src/components/CategoryBar.jsx
@@ -20,24 +20,31 @@ function CategoryBar() {
     { id: 8, name: "Gifts", src: Gifts },
   ];
   return (
-    <div className="h-[8.8rem] w-full text-black bg-gray-100 border-solid border border-gray-100">
-      <div className="max-w-screen-lg flex items-center justify-center mx-auto h-[14rem] px-4 md:flex-row">
-        <div className="justify-center absolute h-[4.9rem]">
-          <ul className="flex flex-row justify-center mx-auto space-x-3">
-            {categories.map(({ id, name, src }) => (
-              <li
-                key={id}
-                className="rounded-full py-2.5 px-4 text-sm bg-white flex flex-row space-x-6"
-              >
-                <div className="pr-2.5 rounded-full h-9 w-9 bg-sky-100 ">
-                  <div className="w-9">
-                    <img src={src} className="" />
-                  </div>
+    <div
+      className="h-[8.8rem] text-black bg-gray-100 border-solid border 
+    border-gray-100"
+    >
+      <div className="scroll-smooth flex flex-row justify-center">
+        <div
+          className="flex flex-row items-center
+    h-[7rem] my-11  mx-auto overflow-x-scroll overscroll-x-contain container-snap space-x-3"
+        >
+          {/* <ul className="flex flex-row justify-center mx-auto space-x-3 snap-x  "> */}
+          {categories.map(({ id, name, src }) => (
+            <div
+              key={id}
+              className="rounded-full py-2.5 px-4 text-sm bg-white 
+              flex flex-row space-x-6"
+            >
+              <div className="pr-2.5 rounded-full h-9 w-9 bg-sky-100 ">
+                <div className="w-9">
+                  <img src={src} className="" />
                 </div>
-                <div className="pt-1.5 text-base">{name}</div>
-              </li>
-            ))}
-          </ul>
+              </div>
+              <div className="pt-1.5 text-base">{name}</div>
+            </div>
+          ))}
+          {/* </ul> */}
         </div>
       </div>
     </div>
--- src/components/FoodTypes.jsx
+++ src/components/FoodTypes.jsx
@@ -1,40 +1,77 @@
 import React from "react";
+import FastFood from "../assets/foodtype/fastFood.png";
+import Mexican from "../assets/foodtype/mexican.png";
+import Dessert from "../assets/foodtype/dessert.png";
+import Chicken from "../assets/foodtype/chicken.png";
+import Burger from "../assets/foodtype/burger.png";
+import Soup from "../assets/foodtype/soup.png";
+import Snack from "../assets/foodtype/snack.png";
+import Pizza from "../assets/foodtype/pizza.png";
+import Drinks from "../assets/foodtype/drinks.png";
+import Chinese from "../assets/foodtype/chinese.png";
+import Sandwiches from "../assets/foodtype/sandwiches.png";
+import Smoothie from "../assets/foodtype/smoothie.png";
+import Coffee from "../assets/foodtype/coffee.png";
+import Healthy from "../assets/foodtype/healthy.png";
+import Breakfast from "../assets/foodtype/breakfast.png";
+import Salad from "../assets/foodtype/salad.png";
+import Italian from "../assets/foodtype/italian.png";
+import Seafood from "../assets/foodtype/seafood.png";
+import Barbecue from "../assets/foodtype/barbeque.png";
+import Bakery from "../assets/foodtype/bakery.png";
+import Asian from "../assets/foodtype/asianfood.png";
+import Thai from "../assets/foodtype/thai.png";
 
 const FoodTypes = () => {
   const foods = [
-    { id: 1, name: "Fast Food" },
-    { id: 2, name: "Mexican" },
-    { id: 3, name: "Desserts" },
-    { id: 4, name: "Chicken" },
-    { id: 5, name: "Burgers" },
-    { id: 6, name: "Soup" },
-    { id: 7, name: "Snacks" },
-    { id: 8, name: "Pizza" },
-    { id: 9, name: "Drinks" },
-    { id: 10, name: "Chinese" },
-    { id: 11, name: "Sandwiches" },
-    { id: 12, name: "Smoothie" },
-    { id: 13, name: "Coffee" },
-    { id: 14, name: "Healthy" },
-    { id: 15, name: "Breakfast" },
-    { id: 16, name: "Salad" },
-    { id: 17, name: "Italian" },
-    { id: 18, name: "Seafood" },
-    { id: 19, name: "Barbecue" },
-    { id: 20, name: "Bakery" },
-    { id: 21, name: "Asian" },
-    { id: 22, name: "Thai" },
+    { id: 1, name: "Fast Food", src: FastFood },
+    { id: 2, name: "Mexican", src: Mexican },
+    { id: 3, name: "Desserts", src: Dessert },
+    { id: 4, name: "Chicken", src: Chicken },
+    { id: 5, name: "Burgers", src: Burger },
+    { id: 6, name: "Soup", src: Soup },
+    { id: 7, name: "Snacks", src: Snack },
+    { id: 8, name: "Pizza", src: Pizza },
+    { id: 9, name: "Drinks", src: Drinks },
+    { id: 10, name: "Chinese", src: Chinese },
+    { id: 11, name: "Sandwiches", src: Sandwiches },
+    { id: 12, name: "Smoothie", src: Smoothie },
+    { id: 13, name: "Coffee", src: Coffee },
+    { id: 14, name: "Healthy", src: Healthy },
+    { id: 15, name: "Breakfast", src: Breakfast },
+    { id: 16, name: "Salad", src: Salad },
+    { id: 17, name: "Italian", src: Italian },
+    { id: 18, name: "Seafood", src: Seafood },
+    { id: 19, name: "Barbeque", src: Barbecue },
+    { id: 20, name: "Bakery", src: Bakery },
+    { id: 21, name: "Asian", src: Asian },
+    { id: 22, name: "Thai", src: Thai },
   ];
+  //  mx-[15rem]
+  // left-[15rem] relative flex-shrink-0
   return (
-    <div className="w-full h-screen text-black">
-      <div className="max-w-screen-lg px-[17.5rem] lex w-1/3 h-full">
-        <ul className="flex flex-row">
-          {foods.map(({ id, name }) => (
-            <li key={id} className="flex flex-col">
-              {name}
-            </li>
+    <div className="h-screen text-black">
+      <div className="scroll-smooth flex flex-row justify-center pl-3">
+        <div
+          className="flex flex-row
+       w-[90rem] overscroll-x-contain overflow-x-scroll 
+       top-[9rem] right-[15rem] snap-x container-snap"
+        >
+          {foods.map(({ id, name, src }) => (
+            <div
+              key={id}
+              className="flex mx-auto flex-col justify-center items-center 
+              space-x-6 snap-start space-y-2 py-4 pr-[.5rem]"
+            >
+              <div className="w-9 h-9 relative">
+                <img className="justify-center" src={src} />
+              </div>
+              <div className="truncate text-sm pr-5 top-[.5rem] relative">
+                {name}
+              </div>
+            </div>
           ))}
-        </ul>
+        </div>
       </div>
     </div>
   );
