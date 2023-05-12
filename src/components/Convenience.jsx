dbca68e0484331832314923663d7bd6b96359e29
Erik Trejo Lujano <trejolujano.erik@gmail.com>
2023-05-12 13:46:10 -0700

    testing

--- src/components/Convenience.jsx
+++ src/components/Convenience.jsx
@@ -3,7 +3,7 @@ import Cvs from "../assets/convenience/cvs.jpeg";
 import Riteaid from "../assets/convenience/riteaid.webp";
 import Seven11 from "../assets/convenience/seven11.jpg";
 import Walgreens from "../assets/convenience/walgreens.jpeg";
-import { TbHeart } from "react-icons/Tb";
+import { TbHeart } from "/react-icons/Tb";
 import { AiOutlineStar } from "/react-icons/Ai";
 import { TbChevronLeft, TbChevronRight } from "/react-icons/Tb";
 
