d2b4660ef345f674133f70c01c62a45e4aea02cb
Erik Trejo Lujano <trejolujano.erik@gmail.com>
2023-05-14 23:13:53 -0700

    deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //edits here
   build: {
     rollupOptions: {
       external: ["react", "react-dom", "react-icons"],
