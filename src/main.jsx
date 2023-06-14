49fae29272a5c551232b06b17300283d64fbc43f
Erik Trejo Lujano <trejolujano.erik@gmail.com>
2023-06-13 23:37:49 -0700

    clean up

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 import "./index.css";
-//edits
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
