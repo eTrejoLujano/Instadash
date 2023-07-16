997191fa5941bee9c3acfe93c8e9ecf1fdf3a507
Erik Trejo Lujano <trejolujano.erik@gmail.com>
2023-07-16 00:28:19 -0700

    http

--- src/components/Util/constants.js
+++ src/components/Util/constants.js
@@ -1,3 +1,3 @@
 // export const REQ_URL = "http://localhost:8000";
 export const REQ_URL =
-  "//django-env.eba-ank3qwa5.us-west-2.elasticbeanstalk.com";
+  "http://django-env.eba-ank3qwa5.us-west-2.elasticbeanstalk.com";
