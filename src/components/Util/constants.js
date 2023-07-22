782263c401d4cf802d671b3b3a11a375a9a70bc8
Erik Trejo Lujano <trejolujano.erik@gmail.com>
2023-07-22 00:41:09 -0700

    prod api ready

--- src/components/Util/constants.js
+++ src/components/Util/constants.js
@@ -1,3 +1,2 @@
 // export const REQ_URL = "http://127.0.0.1:8000";
-export const REQ_URL =
-  "http://django-env.eba-ank3qwa5.us-west-2.elasticbeanstalk.com";
+export const REQ_URL = "http://react.dashedeatsdjango.com";
