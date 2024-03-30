244bf70889d87beb7f26ad988162675e0dc20ba8
Erik Trejo Lujano <trejolujano.erik@gmail.com>
2024-03-30 12:45:20 -0700

    render django url

--- src/components/Util/constants.js
+++ src/components/Util/constants.js
@@ -1,2 +1,3 @@
 // export const REQ_URL = "http://127.0.0.1:8000";
-export const REQ_URL = "https://react.dashedeatsdjango.com";
+// export const REQ_URL = "https://react.dashedeatsdjango.com";
+export const REQ_URL = "https://dashed-eats-django.onrender.com";
